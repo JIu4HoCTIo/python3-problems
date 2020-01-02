a38df0723ea317aa78f5d60b5038dc07d99b45d6
JIu4HoCTIo <43137427+AndreyViziter@users.noreply.github.com>
2020-01-02 15:58:02 +0300

    Update 2.py

--- solutions/2.py
+++ solutions/2.py
@@ -1,11 +1,2 @@
-
-
 def is_year_leap(year):
-
-    if year % 400 == 0:
-        return True
-
-    if year % 4 == 0 and year % 100 != 0:
-        return True
-
-    return False
+    return True if year % 400 == 0 or year % 4 == 0 and year % 100 != 0 else False
