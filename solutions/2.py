b10c3c5f86190fee5a887ddf6e28c5981319ad2d
JIu4HoCTIo <43137427+AndreyViziter@users.noreply.github.com>
2020-01-02 15:53:30 +0300

    Update 2.py

--- solutions/2.py
+++ solutions/2.py
@@ -1,11 +1,3 @@
-
-
 def is_year_leap(year):
-
-    if year % 400 == 0:
-        return True
-
-    if year % 4 == 0 and year % 100 != 0:
-        return True
-
-    return False
+    return True if year % 400 == 0 or year % 4 == 0 and year % 100 != 0 else False
+print(is_year_leap(2019))
